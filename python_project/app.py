6f141ee15e615106af840db3dc6de075c3b683cc
Roman Slaap <romanslaap@gmail.com>
2021-11-07 22:41:28 +0100

    returned to hello world python app

--- python_project/app.py
+++ python_project/app.py
@@ -1,18 +1,9 @@
-# Temporary code to test docker pipeline
-# Stolen from: https://medium.datadriveninvestor.com/deploying-flask-web-app-on-microsoft-azure-89cea17e9114
-from flask import Flask, request, jsonify, render_template, url_for
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello World'
+def index():
+    return 'Web App with Python Flask!'
 
-@app.route('/predict_api', methods=["GET","POST"])
-def list_post():
-    json_body = request.get_json()
-    predictions = 2 * json_body[0]   
-    predictions = list(predictions)
-    return jsonify(results = predictions)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+app.run(host='0.0.0.0', port=81)
